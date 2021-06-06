a18497828c8dbe0b65fcbb1869bca1c1d73b4069
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-06 23:23:17 +0800

    add cart number

--- comps/molecules/navigation/NavList.js
+++ comps/molecules/navigation/NavList.js
@@ -7,25 +7,34 @@ const NavList = () => {
             <ul className='navigation__nav'>
                 <NavLink link='#'>
                     <div className='navigation__nav-icon'>
-                        <HouseDoor size={27} />
+                        <HouseDoor size={28} />
                     </div>
                 </NavLink>
 
                 <NavLink link='#'>
                     <div className='navigation__nav-icon'>
-                        <Search size={27} />
+                        <Search size={28} />
                     </div>
                 </NavLink>
 
                 <NavLink link='#'>
                     <div className='navigation__nav-icon'>
-                        <Cart size={27} />
+                        <div className='navigation__nav-cart'>
+                            <Cart size={28} />
+                            <div className='cart__number-circle'>
+                                <div className='cart__number-content'>
+                                    <span className='cart__number-text'>
+                                        2
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </NavLink>
 
                 <NavLink link='#'>
                     <div className='navigation__nav-icon'>
-                        <Wallet size={27} />
+                        <Wallet size={28} />
                     </div>
                 </NavLink>
             </ul>
