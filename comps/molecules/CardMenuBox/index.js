4d6e0aa29681c1418be84a1f59d6a469ca998ed3
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-08 20:07:36 +0800

    add box shadow

--- comps/molecules/CardMenuBox/index.js
+++ comps/molecules/CardMenuBox/index.js
@@ -1,25 +1,31 @@
 import Price from '../../atomics/cardMenu/Price'
 import LabelCustom from '../../atomics/LabelCustom'
-import Title from '../../atomics/Title'
+import Title from '../../atomics/Title';
+import Button from '../../atomics/form/Button';
 
-const CardMenuBox = ({ classes, link, title, price, discount, bestseller, image  }) => {
+const CardMenuBox = ({ classes, link, onClickCart, title, price, discount, bestseller, image  }) => {
     return (
         <div className={classes ? `card__menubox ${classes}` : 'card__menubox'}>
-            <div className='card__menubox-heading margin-bottom-4'>
+            <div className='card__menubox-heading'>
                 {discount && <LabelCustom classes='card__menubox-label-discount' text={`-${discount}%`} />}
                 {bestseller && <LabelCustom classes='card__menubox-label-bestseller' text='terlaris' />}
                 <img src={image} />
             </div>
 
             <div className='card__menubox-body'>
-                <Title
-                    component='h5'
-                    href={link}
-                    classes='margin-bottom-0'
-                    text={title}
-                    classesLink='card__menubox-link'
-                />
-                <Price price={price} discount={discount} />
+                <div className='card__menubox-body-left'>
+                    <Title
+                        component='h5'
+                        href={link}
+                        classes='margin-bottom-0'
+                        text={title}
+                        classesLink='card__menubox-link'
+                    />
+                    <Price price={price} discount={discount} />
+                </div>
+                <div className='card__menubox-body-right'>
+                    <Button text='+' onClick={onClickCart} />
+                </div>
             </div>
         </div>
     )
