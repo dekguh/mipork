91cccd438e4691a2ccdd9b6972d24e94dce0090b
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-08 13:51:24 +0800

    add sample card menu box

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import CounterDown from '../comps/atomics/CounterDown';
+import CardMenuBox from '../comps/molecules/CardMenuBox';
 import TitleSection from '../comps/molecules/TitleSection';
 import FormSearch from '../comps/organisms/FormSearch';
 import Navigation from '../comps/organisms/Navigation';
@@ -9,9 +10,17 @@ export default function Home() {
     <div>
       <div className='container'>
         <FormSearch />
-        <TitleSection classes='margin-top-16' title='Flash Deal'>
+        <TitleSection classes='margin-top-16 margin-bottom-12' title='Flash Deal'>
           <CounterDown timestamp={1623640142000} />
         </TitleSection>
+        <CardMenuBox
+          image='/images/food/hamburger-cheese.jpg'
+          price={15000}
+          discount={20}
+          bestseller={true}
+          title='Hamburger Cheese'
+          link='#'
+        />
       </div>
       <Navigation />
     </div>
