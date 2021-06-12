f0823b11b65e2569efc5c575f7fcd7011d954bd4
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-12 10:00:31 +0800

    create notif page

---
+++ pages/notification/index.js
@@ -0,0 +1,11 @@
+import NotifSection from "../../comps/organisms/NotifSection"
+
+const NotificationPage = () => {
+    return (
+        <div className='container margin-top-20'>
+            <NotifSection />
+        </div>
+    )
+}
+
+export default NotificationPage
