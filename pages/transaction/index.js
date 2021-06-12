99412c9133a36561d6d075b11b5af73ced28301d
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-12 11:46:40 +0800

    create transaction page

---
+++ pages/transaction/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TrasanctionSection from '../../comps/organisms/TransactionSection';
+
+const TransactionPage = () => {
+    return (
+        <div className='container margin-top-20 margin-bottom-80'>
+            <TrasanctionSection />
+        </div>
+    )
+}
+
+export default TransactionPage
