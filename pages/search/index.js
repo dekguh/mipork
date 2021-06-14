2ad850b9ef406a50235151b97511b982a29fe034
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-14 13:27:46 +0800

    create search page

---
+++ pages/search/index.js
@@ -0,0 +1,11 @@
+import SearchResultSection from "../../comps/organisms/SearchResultSection"
+
+const SearchPage = () => {
+    return (
+        <div className='container margin-top-20 margin-bottom-80'>
+            <SearchResultSection />
+        </div>
+    )
+}
+
+export default SearchPage
